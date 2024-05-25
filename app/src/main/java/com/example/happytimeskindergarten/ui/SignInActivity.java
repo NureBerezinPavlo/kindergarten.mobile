7b8830f02615d42a80e22c5853ed8eef40d07d45
NikitaG25 <nikita.holokha@nure.ua>
2024-05-25 12:02:08 +0300

    sdsd

--- app/src/main/java/com/example/happytimeskindergarten/ui/SignInActivity.java
+++ app/src/main/java/com/example/happytimeskindergarten/ui/SignInActivity.java
@@ -30,25 +30,25 @@ public class SignInActivity extends AppCompatActivity {
         {
             public void onClick(View view)
             {
-/*
+
                 Request.requestUser.login(email.getText().toString(),password.getText().toString(),"asasas").enqueue(new Callback<Auth>() {
                     @Override
                     public void onResponse(Call<Auth> call, Response<Auth> response) {
                         User.setToken(response.body().token);
                         User.setId(response.body().id);
                         User.setFamily_account_id(response.body().family_account_id);
-*/
+
                         // тут надо прописать условие, при котором осуществится переход в соответствующий аккаунт
                         Intent intent = new Intent(SignInActivity.this, MainActivity.class);
                         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                         startActivity(intent);
-                    /*}
+                    }
 
                     @Override
                     public void onFailure(Call<Auth> call, Throwable t) {
                         Toast.makeText(getApplicationContext(), "Некоректні логін або пароль", Toast.LENGTH_SHORT).show();
                     }
-                });*/
+                });
 
 
             }
